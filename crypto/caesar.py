d122bfe10800029df46bace7ce1ff65bb3e34f17
l2mIam <l2m@uw.edu>
2017-05-08 10:42:37 -0700

    Starter code for crypto

---
+++ crypto/caesar.py
@@ -0,0 +1,41 @@
+"""
+LaunchCode lc101 Crypto assignment due 5/14/2017 11:59pm
+Caesar
+"""
+__author__ = "Loren Milliman"
+__date__ = "5/8/2017"
+
+
+def alphabet_position(letter):
+    """ takes a letter and returns the numeric value (a/A = 0 to z/Z = 25)
+    PARAM1: letter (string): A single char (a-Z)
+    RETURN: number (int): {0-25}
+    """
+
+    # get the Ascii value and mod by 26
+    return number
+
+def rotate_character(char, rot):
+    """ takes a character and rotation and returns a characters that is the
+    result of rotating char by rot
+    PARAM1: char (str): A single char (a-Z)
+    PARMA2: rot (int): How far to rotate
+    RETURN: rot_char (str): The result of rotating from char
+    """
+
+    # Use alphabet_position, add rot, mod by 26
+    # get char from num
+    # maybe create a new helper method, position_to_char(number)?
+
+    return rot_char
+
+def main():
+    # write some tests here to test methods
+    # test edge cases: a, A, z, Z
+    # and one or two more: c,t
+    # e.g. alphabet_position(a) should return 0
+    
+    return None
+
+if __name__ == '__main__':
+    main()
---
+++ crypto/vigenere.py
@@ -0,0 +1,8 @@
+"""
+LaunchCode lc101 Crypto assignment due 5/14/2017 11:59pm
+Vigenere
+"""
+__author__ = "Loren Milliman"
+__date__ = "5/8/2017"
+
+
