10d4ee08e0d481c04910b0c9261ba9945bb2ccd9
l2mIam <l2m@uw.edu>
2017-05-18 10:33:29 -0700

    rotate_character should retain case and return accordingly.  Added is_upper and modified position_to_char

--- crypto/caesar.py
+++ crypto/caesar.py
@@ -13,21 +13,30 @@ def alphabet_position(letter):
     """
     return (ord(letter) - 1) % 32
 
-def position_to_char(number):
-    """ takes a position [0,25] and returns character [a,z]
+def position_to_char(number, upper):
+    """ takes a position [0,25] and returns character [a,z] or [A,Z]
     PARAM1: number (int): a single integer [0,25]
-    RETURN: char (string): a single character [a,z]
+    PARAM2: upper (bool): True if upper case
+    RETURN: char (string): a single character [a,z], if upper: [A,Z]
     """
-    return chr(number + 97)
+    mod = 65 if upper else 97
+    return chr(number + mod)
+
+def is_upper(char):
+    """ takes a single character and returns true if uppercase
+    PARAM1: char (string): a single charager [a,z] or [A,Z]
+    RETURN: (bool): true if char [A,Z]
+    """
+    return ord(char) < 97
 
 def rotate_character(char, rot):
     """ takes a character and rotation and returns a characters that is the
     result of rotating char by rot
     PARAM1: char (str): A single char [a,z] or [A,Z]
     PARMA2: rot (int): How far to rotate
-    RETURN: rot_char (str): A single character [a,z] the result of rotating from char
+    RETURN: rot_char (str): A single character [a,z] or [A,Z] the result of rotating from char
     """
-    return position_to_char((alphabet_position(char) + rot) % 26)
+    return position_to_char((alphabet_position(char) + rot) % 26, is_upper(char))
 
 def main():
     """ Just some tests
@@ -41,13 +50,20 @@ def main():
     assert alphabet_position('G') == 6
 
     # Tests for position_to_char
-    assert position_to_char(0) == 'a'
-    assert position_to_char(25) == 'z'
-    assert position_to_char(4) == 'e'
+    assert position_to_char(0, False) == 'a'
+    assert position_to_char(25, False) == 'z'
+    assert position_to_char(4, True) == 'E'
+
+    # Tests for is_upper
+    assert is_upper('A')
+    assert is_upper('Z')
+    assert not is_upper('a')
+    assert not is_upper('z')
 
     # Tests for rotate_character
-    assert rotate_character('A', 5) == 'f'
+    assert rotate_character('A', 5) == 'F'
     assert rotate_character('z', 3) == 'c'
+    assert rotate_character('Y', 28) == 'A'
 
     return None
 
